aeede69228b5919f8d7731233c9e95692b87a2d6
vitmvit <120332119+vitmvit@users.noreply.github.com>
2024-10-30 15:11:31 +0300

    Revert "Dev"

--- src/main/java/ru/clevertec/news/exception/CacheNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.clevertec.news.exception;
-
-public class CacheNotFoundException extends RuntimeException {
-
-    public CacheNotFoundException() {
-        super("Cache not found!");
-    }
-}
